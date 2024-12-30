3283fde44969f636a554b3635c704cc2ecf5a3d2
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2024-12-30 22:12:34 +0300

    editing productActivity

--- app/src/main/java/com/example/shopping_app/Adapter/ImageSliderAdapter.java
+++ app/src/main/java/com/example/shopping_app/Adapter/ImageSliderAdapter.java
@@ -1,4 +1,46 @@
 package com.example.shopping_app.Adapter;
 
-public class ImageSliderAdapter {
+import android.graphics.Bitmap;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.ViewHolder> {
+    private ArrayList<Bitmap> images;
+
+    public ImageSliderAdapter(ArrayList<Bitmap> images) {
+        this.images = images;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ImageView imageView = new ImageView(parent.getContext());
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        ));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        return new ViewHolder(imageView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.imageView.setImageBitmap(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        ViewHolder(ImageView view) {
+            super(view);
+            imageView = view;
+        }
+    }
 }
--- app/src/main/java/com/example/shopping_app/ProductActivity.java
+++ app/src/main/java/com/example/shopping_app/ProductActivity.java
@@ -1,24 +1,43 @@
 package com.example.shopping_app;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.shopping_app.Adapter.ImageSliderAdapter;
+
+import java.util.ArrayList;
 
 public class ProductActivity extends AppCompatActivity {
-
+    ViewPager2 vp = findViewById(R.id.image_slider);
+    TextView title = findViewById(R.id.title);
+    TextView price = findViewById(R.id.price);
+    TextView description = findViewById(R.id.description);
+    Spinner size_spinner = findViewById(R.id.size_spinner);
+    Spinner color_spinner = findViewById(R.id.color_spinner);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_product);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Bundle bundle = getIntent().getExtras();
+        title.setText(bundle.getString("title"));
+        price.setText(bundle.getString("price"));
+        description.setText(bundle.getString("description"));
+        ArrayList<Bitmap> images = bundle.getParcelableArrayList("images");
+
+        ImageSliderAdapter adapter = new ImageSliderAdapter(images);
+        vp.setAdapter(adapter);
+
+
+
     }
 }
