4417cfff921d6a3243cec72be1aa2033bfb83be4
EZZALDEEN E A YOUNIS <ezzaldeeneayounis@post.mu.edu.tr>
2025-01-01 02:32:55 +0300

    Enhancements on ProductActivity (Back Button,Images Adapter and Quantity Increasing,Decreaseing) And Enhancements on Item Clicks at Home Screen (no the Heart symbol is separated ) By Ezzaldeen

--- app/src/main/java/com/example/shopping_app/Activities/ProductActivity.java
+++ app/src/main/java/com/example/shopping_app/Activities/ProductActivity.java
@@ -1,51 +1,99 @@
 package com.example.shopping_app.Activities;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.shopping_app.Adapter.ImageSliderAdapter;
 import com.example.shopping_app.Model.ItemListModel;
 import com.example.shopping_app.R;
 import com.example.shopping_app.ViewModel.ItemListViewModel;
 
+import java.util.ArrayList;
+
 public class ProductActivity extends AppCompatActivity {
     ViewPager2 vp;
     TextView title;
+    ImageButton BackBtn;
     TextView price;
     TextView description;
     Spinner size_spinner;
     Spinner color_spinner;
+    Button increaseQuantityBtn;
+    Button decreaseQuantityBtn;
+
+
+    Integer Quantity =1;
+    TextView QuantityTxt;
     ItemListViewModel itemListViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        BackBtn= (ImageButton) findViewById(R.id.back_button);
+        increaseQuantityBtn=(Button)findViewById(R.id.quantity_increase);
+        decreaseQuantityBtn=(Button)findViewById(R.id.quantity_decrease);
+        QuantityTxt=findViewById(R.id.quantity_text);
+
         vp = findViewById(R.id.image_slider);
         title = findViewById(R.id.title);
         price = findViewById(R.id.price);
         description = findViewById(R.id.description);
         size_spinner = findViewById(R.id.size_spinner);
         color_spinner = findViewById(R.id.color_spinner);
-        try {
+
 
             itemListViewModel=new ItemListViewModel();
             ItemListModel item = itemListViewModel.getItemById(getIntent().getStringExtra("Item ID"));
             title.setText(item.getName());
             price.setText(item.getPrice());
             description.setText(item.getDescription());
+            BackBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
 
-        }catch (Exception e){
-            Log.e("ProductDetails init",e.getMessage());
+        ArrayList<Bitmap> images = new ArrayList<>(); //Adding the same image of the item five times
+        for (int i = 0; i <5 ; i++) {
+            images.add(item.getImg());
         }
-        //Images not implemented yet the item has one image item.getimg();
-//        ArrayList<Bitmap> images = item.getParcelableArrayList("images");
-//
-//        ImageSliderAdapter adapter = new ImageSliderAdapter(images);
-//        vp.setAdapter(adapter);
+        ImageSliderAdapter adapter = new ImageSliderAdapter(images);
+        vp.setAdapter(adapter);
+
+
+        //Quantity adding,removing implementation
+//        try {
+            decreaseQuantityBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Quantity > 1) {
+                        Quantity -= 1;
+                        QuantityTxt.setText(Quantity.toString());
+
+                    }
+
+                }
+            });
+            increaseQuantityBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Quantity += 1;
+                    QuantityTxt.setText(Quantity.toString());
+                }
+            });
+//        }catch (Exception e ){
+//            Log.e("Quantitiy",e.getMessage());
+//        }
 
 
 
